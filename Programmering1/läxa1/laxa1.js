b133b9ee877316f0c1b3eca4e5bf0ed6e5eac951
lolarssonnadolskiit19a <70582618+lolarssonnadolskiit19a@users.noreply.github.com>
2020-10-08 09:33:32 +0200

    läxa

---
+++ Programmering1/läxa1/laxa1.js
@@ -0,0 +1,15 @@
+// variabler
+
+let name=prompt("vad heter du?")
+let alder= prompt("Ange din ålder: ")
+let adress=prompt("ange din andress")
+let postnummer=prompt ("ange ditt postnummer")
+let stad =prompt ("vilken stad bor du i?")
+let nummer =prompt ("Vad är ditt telefon nummer?")
+
+console.log (`jag heter ${name}`)
+console.log (`jag är ${alder} år`)
+console.log (`min ${adress} är`)
+console.log (`mitt postnummer är ${postnummer}`)
+console.log (`jag bor i ${stad}`)
+console.log(`mitt ${nummer} är`)
